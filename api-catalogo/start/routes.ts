da61a691257f3f19ae029531e008545bf4549b4e
Victor Jaber <victorjaber19@gmail.com>
2023-11-17 11:27:48 -0300

    Fix

--- api-catalogo/start/routes.ts
+++ api-catalogo/start/routes.ts
@@ -5,9 +5,13 @@ Route.post('login', async ({ auth, request, response }) => {
   const password = request.input('password')
 
   try {
+    console.log('Attempting login with:', email, password) // Registro para verificar os dados recebidos
+
     const token = await auth.use('api').attempt(email, password)
+    console.log('Token generated:', token) // Registro para verificar o token gerado
     return token
-  } catch {
+  } catch (error) {
+    console.error('Authentication error:', error.message) // Registro para verificar erros de autenticação
     return response.unauthorized('Invalid credentials')
   }
 })
